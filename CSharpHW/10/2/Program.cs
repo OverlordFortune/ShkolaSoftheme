922a056647c3d970f1513c850ddfccf19c7426a3
Artur <overlordfortune@gmail.com>
2018-06-23 12:25:04 +0300

    Add 10HW

---
+++ CSharpHW/10/2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._2
+{
+    class Program
+    {
+        static void swap(ref int a, ref int b)
+        {
+            int c = a;
+            a = b;
+            b = c;
+        }
+        static void InsertionSort(int[] array)
+        {
+            int key, position;
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                key = array[i + 1];
+                position = i;
+                while ((position >= 0) && (array[position] > key))
+                {
+                    array[position + 1] = array[position];
+                    position--;
+                }
+                array[position + 1] = key;
+            }
+        }
+        static void ShowArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("{0} ", array[i]);
+            }
+            Console.Write('\n');
+        }
+        static void Main(string[] args)
+        {
+            int[] array = new int[20];
+            Random rand = new Random();
+            for (int i = 0; i < array.Length; i++)//заполнение масива
+                array[i] = rand.Next(-100, 100);
+
+            ShowArray(array);//масив до сортировки 
+
+            InsertionSort(array);//сортировка масива
+
+            ShowArray(array);//масив после сортировки
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CSharpHW/10/3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._3
+{
+    class MyArray<T>
+    {
+        private T[] array = new T[0];
+        public void Add(T NewElement)
+        {
+            Array.Resize(ref array, array.Length + 1);
+            array[array.Length - 1] = NewElement;
+        }
+        public T GetByIndex(int index)
+        {
+            return this[index];
+        }
+        public T this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < array.Length)
+                    return array[index];
+                else throw new IndexOutOfRangeException();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
