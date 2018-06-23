9c24c9237692f83558bac02893d5d368ff2c2212
Artur <overlordfortune@gmail.com>
2018-06-23 20:29:52 +0300

    Add 11HW

---
+++ CSharpHW/11/1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11._1
+{
+    public class Photo
+    {
+
+    }
+
+    public class Printer
+    {
+        public virtual void Print(string str)
+        {
+            Console.WriteLine("Now we Print {0}", str);
+        }
+    }
+
+    public class ColorPrinter : Printer
+    {
+        public void Print(string text, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine("Wse color print", text);
+            Console.ResetColor();
+        }
+    }
+
+    public class PhotoPrinter : Printer
+    {
+        public void Print (Photo photo)
+        {
+            Console.WriteLine("Now we prit photo");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "Test string";
+
+            Printer printer = new Printer();
+            printer.Print(test);
+
+            ColorPrinter colorPrinter = new ColorPrinter();
+            colorPrinter.Print(test);
+            colorPrinter.Print(test, ConsoleColor.Blue);
+
+            PhotoPrinter photoPrinter = new PhotoPrinter();
+            photoPrinter.Print(test);
+            photoPrinter.Print(new Photo());
+        }
+    }
+}
---
+++ CSharpHW/11/2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _11._1;
+
+namespace _11._2
+{
+    static class MyExstensionsMethods
+    {
+        public static void Print(this ColorPrinter[] colorPrinter, string text, ConsoleColor color = ConsoleColor.Black)
+        {
+            foreach(var item in colorPrinter)
+            item.Print(text, color);
+        }
+
+        public static void Print(this PhotoPrinter[] photoPrinter,params Photo[] photos)
+        {
+            int i = photoPrinter.Length;
+            int j = 0;
+            if (i != 0)
+           foreach(var photo in photos)
+            {
+                    Console.WriteLine("Печатаем {0}-ю фотографию на {1}- принтере", j + 1, (j % i) + 1);
+                    photoPrinter[j % i].Print(photo);
+                    j++;                  
+            }
+        }
+
+        public static void PrintArray(this Printer printer, string[] texts)
+        {
+            if (texts != null && texts.Length != 0)
+                foreach (var item in texts)
+                    printer.Print(item);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ColorPrinter colorPrinter = new ColorPrinter();
+            ColorPrinter[] colorArray =
+            {
+                new ColorPrinter(),
+                new ColorPrinter(),
+                new ColorPrinter()
+            };
+            //colorPrinter.PrintArray();
+            colorArray.Print("Hello", ConsoleColor.Blue);
+
+            PhotoPrinter[] photoPrinter =
+            {
+                new PhotoPrinter(),
+                new PhotoPrinter(),
+                new PhotoPrinter()
+            };
+            photoPrinter.Print(new Photo(), new Photo(), new Photo(), new Photo());
+
+            Printer printer = new Printer();
+            string[] str =
+            {
+                "1", "2", "3"
+            };
+            printer.PrintArray(str);
+            Console.ReadKey();
+        }
+    }
+}
