cb5bb6414400c95ea5d2c125504f92f4cdf1eb9e
Artur <overlordfortune@gmail.com>
2018-06-25 00:56:10 +0300

    Add 14 HW

---
+++ CSharpHW/14/Lottery.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+
+namespace _14._1
+{
+    class Lottery
+    {
+        private int[] numbers = new int[6];
+        private int[] randnumbers = new int[6];
+
+        public int Length
+        {
+            get
+            {
+                return numbers.Length;
+            }
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < 9)
+                    return numbers[index];
+                else throw new IndexOutOfRangeException();
+            }
+            set
+            {
+                if (index >= 0 && index < 9)
+                    numbers[index] = value;
+                else throw new IndexOutOfRangeException();
+            }
+        }
+
+        public bool IsWin()
+        {
+            if (Points() == 0) return false;
+            return true;
+        }
+
+        public int Points()
+        {
+            randnumbers = new int[this.Length];
+            Random rand = new Random();
+            int points = 0;
+            for (int i = 0; i < this.Length; i++)
+            {
+                randnumbers[i] = rand.Next(9);
+                if (numbers[i] == randnumbers[i])
+                {
+                    points++;
+                    Console.WriteLine("{0} - coincided", i + 1);
+                }
+            }
+
+            ShowResult();
+            return points;
+        }
+
+        private void ShowResult()
+        {
+
+            for (int i = 0; i < this.Length; i++)
+            {
+                Console.Write(numbers[i]);
+            }
+            Console.WriteLine("Your numbers");
+
+            for (int i = 0; i < this.Length; i++)
+            {
+                Console.Write(randnumbers[i]);
+            }
+            Console.WriteLine("Rand numbers");
+        }
+    }
+}
---
+++ CSharpHW/14/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _14._1
+{   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lottery lottery = new Lottery();
+
+            for (int i = 0; i < lottery.Length; i++)
+            {
+                Console.WriteLine("Now write {0}- number", i + 1);
+                lottery[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine(lottery.IsWin());
+        }
+    }
+}
