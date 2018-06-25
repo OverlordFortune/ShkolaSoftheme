22761b2eb014bbf0e00facc017cc650616428fcb
Arthur <overlordfortune@gmail.com>
2018-06-26 00:15:21 +0300

    Add 18HW not full
    1e задание

---
+++ CSharpHW/18/1/MobileAccount.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _18._1
+{
+    class MobileAccount
+    {
+        protected readonly string number;
+        protected static Dictionary<string, MobileAccount> accounts = new Dictionary<string, MobileAccount>();
+
+        public MobileAccount(string Number)
+        {
+            this.number = Number;
+            MobileAccount.accounts.Add(Number, this);
+        }
+
+        public string PhoneNumber
+        {
+            get
+            {
+                return number;
+            }
+        }
+
+        public void SendMail(string Number)
+        {
+            Console.WriteLine("Send mail from{0} to {1}", this.PhoneNumber, Number);
+            MobileAccount account;
+            if (MobileAccount.accounts.TryGetValue(Number, out account))
+            {
+                account.GetMail();
+            }
+        }
+
+        public void SendMail(MobileAccount account)
+        {
+            SendMail(account.PhoneNumber);
+        }
+
+        public void Call(string Number)
+        {
+            Console.WriteLine("Call from{0} to {1}", this.PhoneNumber, Number);
+            MobileAccount account;
+            if (MobileAccount.accounts.TryGetValue(Number, out account))
+            {
+                account.GetCall();
+            }
+        }
+
+        public void Call(MobileAccount account)
+        {
+            Call(account.PhoneNumber);
+        }
+
+        public void GetMail()
+        {
+            Console.WriteLine("Get mail {0}", this.PhoneNumber);
+        }
+
+        public void GetCall()
+        {
+            Console.WriteLine("Get call {0}", this.PhoneNumber);
+        }
+    }
+}
---
+++ CSharpHW/18/1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _18._1
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MobileAccount mobile1 = new MobileAccount("0993422226");
+            MobileAccount mobile2 = new MobileAccount("0502121212");
+
+            mobile2.Call("099");
+            mobile2.SendMail("0993422226");
+        }
+    }
+}
