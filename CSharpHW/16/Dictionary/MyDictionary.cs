b36c16e47d83d94e098a9fc1601f0ffd784ea005
Arthur <overlordfortune@gmail.com>
2018-06-25 17:21:08 +0300

    Доделал 16
    Dictionary

---
+++ CSharpHW/16/Dictionary/MyDictionary.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16._3
+{
+    class MyDictionary<TKey, TValue>
+    {
+        List<TKey> keys = new List<TKey>();
+        List<TValue> values = new List<TValue>();
+
+        public void Add(TKey key, TValue value)
+        {
+            if (keys.Contains(key)) throw new Exception("Key has been added");
+            keys.Add(key);
+            values.Add(value);
+        }
+
+        public void Add(KeyValuePair<TKey, TValue> keyvalue)
+        {
+            this.Add(keyvalue.Key, keyvalue.Value);
+        }
+
+        public void Remove(TKey key)
+        {
+            int index = keys.IndexOf(key);
+            if (index >= 0)
+            {
+                values.RemoveAt(index);
+                
+
+                keys.RemoveAt(index);
+                foreach (var item in keys.ToArray())
+                    Console.WriteLine(item);
+                foreach (var item in values.ToArray())
+                    Console.WriteLine(item);
+            }
+        }
+
+        public TValue[] Values
+        {
+            get
+            {
+                return values.ToArray();
+            }
+        }
+
+        public TKey[] Keys
+        {
+            get
+            {
+                return keys.ToArray();
+            }
+        }
+
+
+    }
+}
---
+++ CSharpHW/16/Dictionary/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16._3
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyDictionary<int, string> dictionary = new MyDictionary<int, string>();
+            Random rand = new Random();
+            try
+            {
+                int i = 0;
+                int j = 0;
+                Console.WriteLine("Add to Dictionary");
+                Console.WriteLine(new string('-', 30));
+                while (true)
+                {
+                    j = rand.Next(9);
+                    dictionary.Add(j, "Mystring " + i);
+                    Console.WriteLine(j + "  Mystring "+  i);
+                    i++;
+                }
+                
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine(new string('-', 30));
+
+                foreach (var item in dictionary.Keys)
+                    Console.WriteLine(item);
+
+                foreach (var item in dictionary.Values)
+                    Console.WriteLine(item);
+            }              
+        }
+    }
+}
